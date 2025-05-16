3063c1d3033d2a9a41387498314e80a15546b22f
isakidev <gaspar.c.developer@gmail.com>
2025-05-16 02:09:15 -0400

    feat: config ajv

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,20 @@
 import fastify from 'fastify'
+import ajvErrors from 'ajv-errors'
 
+import { errorHandler } from './handlers/errors.handler.ts'
 import { Server } from './server.ts'
 
-const app = fastify()
+const app = fastify({
+  ajv: {
+    customOptions: {
+      $data: true,
+      allErrors: true,
+    },
+    plugins: [ajvErrors],
+  },
+})
+
+app.setErrorHandler(errorHandler())
 
 const server = new Server({
   app,
