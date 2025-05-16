212bda99b3f7c22425fea81e65d7a1c7985f07ed
isakidev <gaspar.c.developer@gmail.com>
2025-05-16 02:14:05 -0400

    feat: implement schemas

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -1,8 +1,14 @@
 import { AuthController } from '../controllers/auth.controller.ts'
+import { LoginSchema } from '../schemas/login.schema.ts'
+import { RegisterSchema } from '../schemas/register.schema.ts'
 
 export class AuthRoute {
   static routes(app: any) {
-    app.get('/login', AuthController.login)
-    app.get('/register', AuthController.register)
+    app.post('/login', { schema: { body: LoginSchema } }, AuthController.login)
+    app.post(
+      '/register',
+      { schema: { body: RegisterSchema } },
+      AuthController.register,
+    )
   }
 }
