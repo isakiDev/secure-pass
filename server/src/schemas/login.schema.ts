ae8f904ae92ec2aa464f763b3ade58619b2288b1
isakidev <gaspar.c.developer@gmail.com>
2025-05-16 02:13:26 -0400

    feat: add validation schemas

---
+++ server/src/schemas/login.schema.ts
@@ -0,0 +1,9 @@
+export const LoginSchema = {
+  type: 'object',
+  required: ['email', 'password'],
+  properties: {
+    email: { type: 'string', format: 'email' },
+    password: { type: 'string', minLength: 8 },
+  },
+  additionalProperties: false,
+} as const
