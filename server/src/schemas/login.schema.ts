c1e8d6f649f60f847bf5ece3510baa40b476282f
isakidev <gaspar.c.developer@gmail.com>
2025-05-17 17:58:14 -0400

    chore: remove unused files

--- server/src/schemas/login.schema.ts
+++
@@ -1,9 +0,0 @@
-export const LoginSchema = {
-  type: 'object',
-  required: ['email', 'password'],
-  properties: {
-    email: { type: 'string', format: 'email' },
-    password: { type: 'string', minLength: 8 },
-  },
-  additionalProperties: false,
-} as const
